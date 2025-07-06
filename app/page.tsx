263e70f68b471de89bd9c106990e60ed95f4de53
imran <imran@fotia.com.my>
2025-07-06 22:28:30 +0800

    update changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get Started</button>
+      <h1>Collect customer feedback to build better products.</h1>
+      <div>Create a feedback  boards in minute, prioritize features, and build a products your customer will loves</div>
     </main>
   );
 }
